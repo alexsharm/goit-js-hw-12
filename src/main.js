07f320f95be2dcebb656f5cec61fb59bff971781
alexsharm <alfex.sharm@gmail.com>
2024-01-18 17:16:27 +0200

    load more and scrolling added

--- src/main.js
+++ src/main.js
@@ -6,26 +6,41 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const loadBtn = document.querySelector('.load-btn');
+
+let totalPages;
+let inputValue;
+let page = 1;
+let perPage = 40;
 
 const galleryLightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
 });
 
 form.addEventListener('submit', handleSubmit);
+loadBtn.addEventListener('click', handleSubmit);
 
 function handleSubmit(evt) {
   evt.preventDefault();
-  gallery.innerHTML = '';
+  if (evt.target != loadBtn) {
+    loadBtn.classList.add('is-hidden');
+    inputValue = evt.target.elements.search.value;
+    gallery.innerHTML = '';
+    page = 1;
+    evt.target.reset();
+  }
   loader.classList.remove('is-hidden');
 
   async function getPhotos() {
     const response = await axios.get(`https://pixabay.com/api/`, {
       params: {
         key: '41849912-0888eabd10c40a0c420151dd5',
-        q: evt.target.elements.search.value.trim(),
+        q: inputValue,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
+        page: page,
+        per_page: perPage,
       },
     });
     return response.data;
@@ -33,6 +48,8 @@ function handleSubmit(evt) {
 
   getPhotos()
     .then(data => {
+      console.log(data);
+      totalPages = Math.ceil(data.totalHits / perPage);
       loader.classList.add('is-hidden');
       if (data.hits.length === 0) {
         const errorAlert = iziToast.error({
@@ -73,11 +90,35 @@ function handleSubmit(evt) {
           )
           .join('');
         gallery.insertAdjacentHTML('beforeend', htmlMarkup);
+        loadBtn.classList.remove('is-hidden');
         galleryLightbox.refresh();
+        if (evt.target === loadBtn) {
+          const galleryCardProperties = document
+            .querySelector('.gallery-item')
+            .getBoundingClientRect();
+          const galleryCardHeight = galleryCardProperties.height;
+          console.log(galleryCardHeight);
+          window.scrollBy({
+            top: 2 * galleryCardHeight,
+            behavior: 'smooth',
+          });
+        }
+        console.log(page);
+        console.log(totalPages);
+        if (page == totalPages) {
+          loadBtn.classList.add('is-hidden');
+          loader.classList.add('is-hidden');
+          return iziToast.error({
+            position: 'topRight',
+            message: `We're sorry, but you've reached the end of search results.`,
+          });
+        }
+        page += 1;
       }
-      evt.target.reset();
     })
     .catch(error => {
       console.log(error);
     });
 }
+
+// inputValue != evt.target.elements.search.value
