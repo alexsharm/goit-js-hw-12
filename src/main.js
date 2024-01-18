4952db8e572a7ee3580513cacac33b62e25f7e4b
alexsharm <alfex.sharm@gmail.com>
2024-01-18 12:31:54 +0200

    http refactoring

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
@@ -16,23 +17,19 @@ function handleSubmit(evt) {
   evt.preventDefault();
   gallery.innerHTML = '';
   loader.classList.remove('is-hidden');
-  const searchParams = new URLSearchParams({
-    key: '41849912-0888eabd10c40a0c420151dd5',
-    q: evt.target.elements.search.value.trim(),
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-  fetch(`https://pixabay.com/api/?${searchParams}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+  axios
+    .get(`https://pixabay.com/api/`, {
+      params: {
+        key: '41849912-0888eabd10c40a0c420151dd5',
+        q: evt.target.elements.search.value.trim(),
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+      },
     })
-    .then(data => {
+    .then(response => {
       loader.classList.add('is-hidden');
-      if (data.hits.length === 0) {
+      if (response.data.hits.length === 0) {
         const errorAlert = iziToast.error({
           message:
             'Sorry, there are no images matching <br> your search query. Please try again!',
@@ -40,7 +37,7 @@ function handleSubmit(evt) {
           class: 'error-alert',
         });
       } else {
-        const htmlMarkup = data.hits
+        const htmlMarkup = response.data.hits
           .map(
             image =>
               `
